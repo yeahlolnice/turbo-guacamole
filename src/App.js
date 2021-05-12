2d11ba22abf7d565c0987e7bfade743541640cc6
yeahlolnice <mattwilly98@hotmail.com>
2021-05-12 19:12:44 +1000

    type writer effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import Welcoming from "./components/welcoming";
 import "./style.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Welcoming />
       <About />
       <Footer />
     </div>
---
+++ src/components/welcoming.jsx
@@ -0,0 +1,8 @@
+function welcome() {
+    return (
+        <div className="welcome">
+            <h3>Matt Williams Developer.</h3>
+        </div>
+    )
+}
+export default welcome;
